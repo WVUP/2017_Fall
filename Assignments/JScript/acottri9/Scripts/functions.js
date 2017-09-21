f3bf91067032ced380de7b659d4b9461e71c6749
Aaron <a.rcottrill521@gmail.com>
2017-09-21 18:24:03 -0400

    Add files via upload
    Right folder now.

--- Assignments/JScript/acottri9/Scripts/functions.js
+++ Assignments/JScript/acottri9/Scripts/functions.js
@@ -8,3 +8,21 @@ function log(id)
     var input = document.getElementById(id);
     console.log(input.value);
 }
+
+function changeImage(imageID,teamID)
+{
+    var image = document.getElementById(imageID);
+    var team = document.getElementById(teamID);
+    if(team.value == "yellow")
+    {
+        image.src = "images/instinct.png";
+    }
+    else if(team.value == "red")
+    {
+        image.src = "images/valor.png";
+    }
+    else if(team.value == "blue")
+    {
+        image.src = "images/mystic.png";
+    }
+}
