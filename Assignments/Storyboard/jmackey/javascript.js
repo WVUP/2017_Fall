74f260d4423676dbbbd8789f690ee9ad7cb4ea2f
jennym1211 <31522533+jennym1211@users.noreply.github.com>
2017-12-07 19:25:01 -0500

    javascriptupdate

--- Assignments/Storyboard/jmackey/javascript.js
+++ Assignments/Storyboard/jmackey/javascript.js
@@ -14,94 +14,100 @@ var html = '';
 
 function addClass(CRN, course, title, credits, startDate, endDate, days, time, instructor) {
 
-    
-    CRN[5415] = { course: "CS101", title: "Intro to PC Applications", credits: 4, startDate: "01-08-2018", endDate: "05-04-2018", days: "T, Th", time: "1:00 PM - 2:15 PM", instructor: "Gary Thompson"};
-    CRN[5424] = { course: "CS121", title: "Intro to Programming", credits: 4, startDate: "01-08-2018", endDate: "05-04-2018", days: "M, W ", time: "11:00 AM - 12:45 PM", instructor: "Charles Almond"};
-    CRN[5405] = { course: "CS122", title: "Object Oriented Programming", credits: 4, startDate: "01-08-2018", endDate: "05-04-2018", days: "M, T, W, Th", time: "9:30 AM - 10:20 AM", instructor: "Gary Thompson"};
-    CRN[5427] = { course: "CS220", title: "Web Application Development", credits: 3, startDate: "01-08-2018", endDate: "05-04-2018", days: "T, Th", time: "11:00 AM - 12:15 PM", instructor: "Charles Almond"};
-    CRN[5536] = { course: "CS260", title: "Computer Science Capstone", credits: 3, startDate: "01-08-2018", endDate: "05-04-2018", days: "T, Th", time: "1:00 PM - 2:15 PM", instructor: "Charles Almond"};
-    
-    
- 
-     var table = document.getElementById("table");
-     
-     var row= document.createElement("tr");
-     console.log(row);
-     var td1 = document.createElement("td");
-     var td2 = document.createElement("td");
-     var td3 = document.createElement("td");    
-     var td4 = document.createElement("td");
-     var td5 = document.createElement("td");
-     var td6 = document.createElement("td");
-     var td7 = document.createElement("td");
- 
 
+  var html = '';
 
+  CRN = [
 
-
-     td1.innerhtml = document.getElementById("CRN").value;
-
-     td1.innerHTML =  document.getElementById("course").value;
-     
-     td2.innerHTML =  document.getElementById("title").value;
-     
-     td3.innerHTML =  document.getElementById("credits").value;
-     td4.innerHTML =  document.getElementById("startDate").value;
-     td5.innerHTML =  document.getElementById("endDate").value;
-     td6.innerHTML =  document.getElementById("days").value;
-     td7.innerHTML =  document.getElementById("time").value;
-     td7.innerHTML =  document.getElementById("instructor").value;
- 
-     row.appendChild(td0);
-     row.appendChild(td1);
-     row.appendChild(td2);
-     row.appendChild(td3);
-     row.appendChild(td4);
-     row.appendChild(td5);
-     row.appendChild(td6);
-     row.appendChild(td7);
-      
-     table.children[0].appendChild(row);
+   {crnNumber: '5415',   course: 'CS101', title: 'Intro to PC Applications', credits: '4', startDate: '01-08-2018', endDate: '05-04-2018', days: 'T, Th', time: '1:00 PM - 2:15 PM', instructor: 'Gary Thompson'},
    
-
+  {crnNumber: '5424', course: 'CS121', title: 'Intro to Programming', credits: 4, startDate: '01-08-2018', endDate: '05-04-2018', days: 'M, W ', time: '11:00 AM - 12:45 PM', instructor: 'Charles Almond'},
+  { crnNumber: '5405', course: 'CS122', title: 'Object Oriented Programming', credits: 4, startDate: '01-08-2018', endDate: '05-04-2018', days: 'M, T, W, Th', time: '9:30 AM - 10:20 AM', instructor: 'Gary Thompson'},
+   { crnNumber: '5427', course: 'CS220', title: 'Web Application Development', credits: 3, startDate: '01-08-2018', endDate: '05-04-2018', days: 'T, Th', time: '11:00 AM - 12:15 PM', instructor: 'Charles Almond'},
+    { crnNumber: '5536', course: 'CS260', title: 'Computer Science Capstone', credits: 3, startDate: '01-08-2018', endDate: '05-04-2018', days: 'T, Th', time: '1:00 PM - 2:15 PM', instructor: 'Charles Almond'}
     
- };
+  ]
 
 
 
 
 
- <p id="demo"></p>
- 
-
- // Set the date we're counting down to
- var countDownDate = new Date("Dec 25").getTime();
- 
- // Update the count down every 1 second
- var x = setInterval(function() {
- 
-   // Get todays date and time
-   var now = new Date().getTime();
- 
-   // Find the distance between now an the count down date
-   var distance = countDownDate - now;
- 
-   // Time calculations for days, hours, minutes and seconds
-   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
- 
-   // Display the result in the element with id="demo"
-   document.getElementById("demo").innerHTML = days + "days " + hours + "hours "
-   + minutes + "minutes" + seconds + "seconds ";
- 
-   // If the count down is finished, write some text 
-   if (distance < 0) {
-     clearInterval(x);
-     document.getElementById("demo").innerHTML = "Merry Christmas!";
-   }
- }, 1000);
+   html += "<tbody>";
+   html += "<tr>";
+   html += "<td> 5415 </td>";
+   html += "<td> CS101 </td>"
+   html += "<td>Intro to PC Applications </td>";
+   html += "<td>4</td>";
+   html += "<td>01-08-2018</td>";
+   html += "<td>05-04-2018</td>";
+   html += "<td>T, Th</td>";
+   html += "<td>1:00 PM - 2:15 PM</td>";
+   html += "<td>Gary Thompson</td>";
+   html += "</tr>";
 
 
- /** https://www.w3schools.com/howto/howto_js_countdown.asp Reference for javascript countdown function. */
+
+   html += "<tr>";
+   html += "<td> 5424 </td>";
+   html += "<td> CS121 </td>"
+   html += "<td>Intro to Programming </td>";
+   html += "<td>4</td>";
+   html += "<td>01-08-2018</td>";
+   html += "<td>05-04-2018</td>";
+   html += "<td>M, W</td>";
+   html += "<td>11:00 AM - 12:45 PM</td>";
+   html += "<td>Charles Almond</td>";
+   html += "</tr>";
+
+
+   html += "<tr>";
+   html += "<td> 5405 </td>";
+   html += "<td> CS122 </td>"
+   html += "<td>Object Oriented Programing</td>";
+   html += "<td>4</td>";
+   html += "<td>01-08-2018</td>";
+   html += "<td>05-04-2018</td>";
+   html += "<td>M, T, W, Th</td>";
+   html += "<td>9:30 PM - 10:20 AM</td>";
+   html += "<td>Gary Thompson</td>";
+   html += "</tr>";
+
+
+
+
+  
+   html += "<tr>";
+   html += "<td> 5424 </td>";
+   html += "<td> C220 </td>"
+   html += "<td>Web Application Development</td>";
+   html += "<td>3</td>";
+   html += "<td>01-08-2018</td>";
+   html += "<td>05-04-2018</td>";
+   html += "<td>M, W</td>";
+   html += "<td>11:00 AM - 12:15 PM</td>";
+   html += "<td>Charles Almond</td>";
+   html += "</tr>";
+
+
+   
+   html += "<tr>";
+   html += "<td> 5424 </td>";
+   html += "<td> C260 </td>"
+   html += "<td>Computer Science Capstone</td>";
+   html += "<td>3</td>";
+   html += "<td>01-08-2018</td>";
+   html += "<td>05-04-2018</td>";
+   html += "<td>T, tH</td>";
+   html += "<td>1:00 PM - 2:15 PM</td>";
+   html += "<td>Charles Almond</td>";
+   html += "</tr>";
+
+
+
+
+   html += "</tbody>";
+
+   
+   document.getElementById('table').innerHTML = html;
+  
+}
